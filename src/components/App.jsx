a2382a5dbe0e193cfaf2d25475f8b984c2969af2
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-15 10:03:34 +0200

    fix my problem

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: `column`,
+        alignItems: `center`,
+        color: '#010101',
+      }}
+    >
+    </div>
+  );
+};
